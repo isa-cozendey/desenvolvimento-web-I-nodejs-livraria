f86034e07bba359b006e3d4704946bb4b4bd0460
isadora-meco <isadora.meco@hotmail.com>
2025-10-16 11:26:19 -0300

    .

--- livraria/src/routes/livros.routes.js
+++ livraria/src/routes/livros.routes.js
@@ -1,11 +1,10 @@
 const express = require("express");
 const router = express.Router();
-// Controllers
 const LivrosController = require("../controllers/livros.controller");
 const livrosController = new LivrosController();
 router.get("/", livrosController.listarLivros.bind(livrosController));
-router.get("/:id", validarParamId, livrosController.buscarLivroPorId.bind(livrosController));
-router.post("/", validarLivro, livrosController.criarLivro.bind(livrosController));
-router.put("/:id", validarParamId, validarLivro, livrosController.atualizarLivro.bind(livrosController));
-router.delete("/:id", validarParamId, livrosController.removerLivro.bind(livrosController));
+router.get("/:id", livrosController.buscarLivroPorId.bind(livrosController));
+router.post("/", livrosController.criarLivro.bind(livrosController));
+router.put("/:id", livrosController.atualizarLivro.bind(livrosController));
+router.delete("/:id", livrosController.removerLivro.bind(livrosController));
 module.exports = router;
